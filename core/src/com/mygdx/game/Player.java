b6fccebdfe10366f280f5fad79203a126616f040
notsign <notsign@outlook.com>
2016-04-11 10:36:39 -0400

    Changed categoryBits so player actually collides with stuff

--- core/src/com/mygdx/game/Player.java
+++ core/src/com/mygdx/game/Player.java
@@ -10,6 +10,7 @@ import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
@@ -22,10 +23,8 @@ import com.badlogic.gdx.scenes.scene2d.Actor;
 
 public class Player extends Actor {
 	State state;
-	Body mainBody;
-	BodyDef bdefMain;
-	FixtureDef fdefPlayer, fdefFoot;
-	PolygonShape shape;
+	Body body;
+	Fixture footSensor;
 	TextureAtlas taIdle = new TextureAtlas(Gdx.files.internal("player/idle/idle.pack"));
 	TextureAtlas taRun = new TextureAtlas(Gdx.files.internal("player/run/run.pack"));
 	Sprite[] sIdle = new Sprite[9];
@@ -52,45 +51,44 @@ public class Player extends Actor {
 	private void createMainBody(Vector2 spawnpoint) {
 		this.state = State.IDLE;
 		for(int i = 1; i < 10; i++) {
-			sIdle[i - 1] = new Sprite(taIdle.findRegion("IDLE (" + i + ")"));
+			sIdle[i - 1] = new Sprite(taIdle.findRegion("idle (" + i + ")"));
 			sRun[i - 1] = new Sprite(taRun.findRegion("run (" + i + ")"));
 		}
 		idle = new Animation(10, sIdle);
 		run = new Animation(5, sRun);
-		bdefMain = new BodyDef();
-		shape = new PolygonShape();
+		BodyDef bodyDef = new BodyDef();
+		PolygonShape shape = new PolygonShape();
 
-		bdefMain.position.set(new Vector2(spawnpoint.x / 2, spawnpoint.y / 2));
-		bdefMain.type = BodyDef.BodyType.DynamicBody;
-		mainBody = world.createBody(bdefMain);
-		mainBody.setFixedRotation(true);
+		bodyDef.position.set(new Vector2(spawnpoint.x / 2, spawnpoint.y / 2));
+		bodyDef.type = BodyDef.BodyType.DynamicBody;
+		body = world.createBody(bodyDef);
+		body.setFixedRotation(true);
 
 		shape.setAsBox(sIdle[0].getWidth() / 4, sIdle[0].getHeight() / 4);
-		fdefPlayer = new FixtureDef();
+		FixtureDef fdefPlayer = new FixtureDef();
 		fdefPlayer.shape = shape;
-		fdefPlayer.filter.categoryBits = 0;
 		fdefPlayer.friction = 1;
-		mainBody.setSleepingAllowed(false);
-		mainBody.createFixture(fdefPlayer);
+		body.setSleepingAllowed(false);
+		body.createFixture(fdefPlayer);
 		shape.dispose();
 		// set categorybit to 0 so it collides with nothing
 	}
 
 	private void createFootSensor() {
-		shape = new PolygonShape();
+		PolygonShape shape = new PolygonShape();
 
-		shape.setAsBox(sIdle[0].getWidth() / 4, 0.2f, new Vector2(mainBody.getWorldCenter().x / 4 - sIdle[0].getWidth() / 4 + 0.5f, mainBody.getPosition().y / 4 - sIdle[0].getHeight() - 9.5f), 0);
-		fdefFoot = new FixtureDef();
-		fdefFoot.shape = shape;
-		fdefFoot.filter.categoryBits = 1;
+		shape.setAsBox(sIdle[0].getWidth() / 4, 0.2f, new Vector2(body.getWorldCenter().x / 4 - sIdle[0].getWidth() / 4 + 0.5f, body.getPosition().y / 4 - sIdle[0].getHeight() - 9.5f), 0);
+		FixtureDef fdefFootSensor = new FixtureDef();
+		fdefFootSensor.shape = shape;
+		fdefFootSensor.isSensor = true;
 
-		mainBody.createFixture(fdefFoot);
+		footSensor = body.createFixture(fdefFootSensor);
 		shape.dispose();
 		// create a foot sensor to detect whether or not the player is grounded
 	}
 
 	Vector3 getPosition() {
-		return new Vector3(mainBody.getPosition().x, mainBody.getPosition().y, 0);
+		return new Vector3(body.getPosition().x, body.getPosition().y, 0);
 	}
 
 	void draw(SpriteBatch sb) {
@@ -98,42 +96,42 @@ public class Player extends Actor {
 		elapsedTime++;
 		if(this.state == State.IDLE) {
 			if(bRight) {
-				sb.draw(idle.getKeyFrame(elapsedTime, true), mainBody.getPosition().x - sIdle[0].getWidth() / 4, mainBody.getPosition().y - sIdle[0].getHeight() / 4, sIdle[0].getWidth() / 2, sIdle[0].getHeight() / 2);
+				sb.draw(idle.getKeyFrame(elapsedTime, true), body.getPosition().x - sIdle[0].getWidth() / 4, body.getPosition().y - sIdle[0].getHeight() / 4, sIdle[0].getWidth() / 2, sIdle[0].getHeight() / 2);
 			} else {
-				sb.draw(idle.getKeyFrame(elapsedTime, true), mainBody.getPosition().x + sIdle[0].getWidth() / 4, mainBody.getPosition().y - sIdle[0].getHeight() / 4, -sIdle[0].getWidth() / 2, sIdle[0].getHeight() / 2);
+				sb.draw(idle.getKeyFrame(elapsedTime, true), body.getPosition().x + sIdle[0].getWidth() / 4, body.getPosition().y - sIdle[0].getHeight() / 4, -sIdle[0].getWidth() / 2, sIdle[0].getHeight() / 2);
 			}
 		} else if(state == State.RIGHT) {
-			sb.draw(run.getKeyFrame(elapsedTime, true), mainBody.getPosition().x - sIdle[0].getWidth() / 4, mainBody.getPosition().y - sIdle[0].getHeight() / 4, sRun[0].getWidth() / 2, sRun[0].getHeight() / 2);
+			sb.draw(run.getKeyFrame(elapsedTime, true), body.getPosition().x - sIdle[0].getWidth() / 4, body.getPosition().y - sIdle[0].getHeight() / 4, sRun[0].getWidth() / 2, sRun[0].getHeight() / 2);
 		} else if(state == State.LEFT) {
-			sb.draw(run.getKeyFrame(elapsedTime, true), mainBody.getPosition().x + sIdle[0].getWidth() / 4, mainBody.getPosition().y - sIdle[0].getHeight() / 4, -sRun[0].getWidth() / 2, sRun[0].getHeight() / 2);
+			sb.draw(run.getKeyFrame(elapsedTime, true), body.getPosition().x + sIdle[0].getWidth() / 4, body.getPosition().y - sIdle[0].getHeight() / 4, -sRun[0].getWidth() / 2, sRun[0].getHeight() / 2);
 		}
 	}
 
 	void move() {
 		// i don't even need to pass a keycode, i just call player.move() in render and i can check
 		// for keypresses by detecting if a button is down or not
-		if(mainBody.getLinearVelocity().x > 100) {
-			mainBody.getLinearVelocity().x--;
-		} else if(mainBody.getLinearVelocity().x < -100) {
-			mainBody.getLinearVelocity().x++;
+		if(body.getLinearVelocity().x > 100) {
+			body.getLinearVelocity().x--;
+		} else if(body.getLinearVelocity().x < -100) {
+			body.getLinearVelocity().x++;
 		}
 		if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
 			this.state = State.LEFT;
 			bRight = false;
-			//mainBody.applyForceToCenter(-200, 0, true);
-			mainBody.setLinearVelocity(-100, mainBody.getLinearVelocity().y);
+			//body.applyForceToCenter(-200, 0, true);
+			body.setLinearVelocity(-100, body.getLinearVelocity().y);
 		} else if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
 			this.state = State.RIGHT;
 			bRight = true;
-			//mainBody.applyForceToCenter(200, 0, true);
-			mainBody.setLinearVelocity(100, mainBody.getLinearVelocity().y);
+			//body.applyForceToCenter(200, 0, true);
+			body.setLinearVelocity(100, body.getLinearVelocity().y);
 		}
 	}
 
 	void stop() {
 		// stop movement on release of keycode
-		this.state = State.IDLE;
-		mainBody.setLinearVelocity(0, mainBody.getLinearVelocity().y);
+		state = State.IDLE;
+		body.setLinearVelocity(0, body.getLinearVelocity().y);
 	}
 
 	void jump() {
@@ -141,11 +139,11 @@ public class Player extends Actor {
 		// gravity sucks
 		// there is one issue: jumping while moving gives you a lower jump height than jumping while standing
 		// used applyForceToCenter and setLinearVelocity, same result
-		mainBody.applyLinearImpulse(new Vector2(0, mainBody.getMass() * 500), mainBody.getWorldCenter(), true);
+		body.applyLinearImpulse(new Vector2(0, body.getMass() * 500), body.getWorldCenter(), true);
 	}
 
 	Vector2 getLinearVelocity() {
-		return mainBody.getLinearVelocity();
+		return body.getLinearVelocity();
 	}
 
 }
