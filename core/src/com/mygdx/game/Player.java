42149d66c33cf3fedcc49c0c0cb0eb75b9b19482
notsign <notsign@outlook.com>
2016-04-15 10:36:13 -0400

    Changed the way touch input is handled (Hopefully better)

--- core/src/com/mygdx/game/Player.java
+++ core/src/com/mygdx/game/Player.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.physics.box2d.Body;
@@ -22,25 +23,17 @@ import com.badlogic.gdx.scenes.scene2d.Actor;
 
 
 public class Player extends Actor {
-	State state;
 	Body body;
 	Fixture footSensor;
 	TextureAtlas taIdle = new TextureAtlas(Gdx.files.internal("player/idle/idle.pack"));
 	TextureAtlas taRun = new TextureAtlas(Gdx.files.internal("player/run/run.pack"));
-	Sprite[] sIdle = new Sprite[9];
-	Sprite[] sRun = new Sprite[9];
 	Animation aniIdle, aniRun;
 	float elapsedTime = 0;
 	World world;
 	final int nFinHealth = 3;
 	public int nCurHealth;
 
-	boolean bRight = true, isGrounded = true;
-
-	enum State {
-		IDLE, LEFT, RIGHT
-		// enumeration for animations
-	}
+	boolean bRight = true, isGrounded = true, isIdle = true;
 
 	Player(World world, Vector2 spawnpoint) {
 		this.world = world;
@@ -49,36 +42,39 @@ public class Player extends Actor {
 	}
 
 	private void createMainBody(Vector2 spawnpoint) {
-		state = State.IDLE;
-		for(int i = 1; i < 10; i++) {
-			sIdle[i - 1] = new Sprite(taIdle.findRegion("idle (" + i + ")"));
-			sRun[i - 1] = new Sprite(taRun.findRegion("run (" + i + ")"));
-		}
-		aniIdle = new Animation(10, sIdle);
-		aniRun = new Animation(5, sRun);
+		aniIdle = new Animation(10, taIdle.getRegions());
+		aniRun = new Animation(5, taRun.getRegions());
+
+		TextureRegion trPlayer = aniIdle.getKeyFrame(0);
+		int width = trPlayer.getRegionWidth();
+		int height = trPlayer.getRegionHeight();
+
 		BodyDef bodyDef = new BodyDef();
 		PolygonShape shape = new PolygonShape();
 
 		bodyDef.position.set(new Vector2(spawnpoint.x / 2, spawnpoint.y / 2));
 		bodyDef.type = BodyDef.BodyType.DynamicBody;
+		bodyDef.fixedRotation = true;
 		body = world.createBody(bodyDef);
-		body.setFixedRotation(true);
 
-		shape.setAsBox(sIdle[0].getWidth() / 4, sIdle[0].getHeight() / 4);
+		shape.setAsBox(width / 4, height / 4);
 		FixtureDef fdefPlayer = new FixtureDef();
 		fdefPlayer.shape = shape;
 		fdefPlayer.friction = 1;
 		body.setSleepingAllowed(false);
 		body.createFixture(fdefPlayer);
 		shape.dispose();
-		// set categorybit to 0 so it collides with nothing
 	}
 
 	private void createFootSensor() {
 		PolygonShape shape = new PolygonShape();
 
-		//shape.setAsBox(sIdle[0].getWidth() / 4, 0.2f, new Vector2(body.getWorldCenter().x / 4 - sIdle[0].getWidth() / 4 + 0.5f, body.getPosition().y / 4 - sIdle[0].getHeight() - 9.5f), 0);
-		shape.setAsBox(sIdle[0].getWidth() / 8, 0.2f, new Vector2(body.getWorldCenter().x / 4 - sIdle[0].getWidth() / 4 + 0.5f, body.getPosition().y / 4 - sIdle[0].getHeight() - 9.5f), 0);
+		TextureRegion trPlayer = aniIdle.getKeyFrame(0);
+		int width = trPlayer.getRegionWidth();
+		int height = trPlayer.getRegionHeight();
+
+		//shape.setAsBox(width / 4, 0.2f, new Vector2(body.getWorldCenter().x / 4 - width / 4 + 0.5f, body.getPosition().y / 4 - height - 9.5f), 0);
+		shape.setAsBox(width / 8, 0.2f, new Vector2(body.getWorldCenter().x / 4 - width / 4 + 0.5f, body.getPosition().y / 4 - height - 9.5f), 0);
 
 		FixtureDef fdefFootSensor = new FixtureDef();
 		fdefFootSensor.shape = shape;
@@ -95,59 +91,56 @@ public class Player extends Actor {
 
 	void draw(SpriteBatch spriteBatch) {
 		// drawing sprite on player body using default library, not using animatedbox2dsprite because it doesn't loop the animation
+
+		TextureRegion trCurrent;
+
+		if (isIdle)
+			trCurrent = aniIdle.getKeyFrame(elapsedTime, true);
+		else
+			trCurrent = aniRun.getKeyFrame(elapsedTime, true);
+
+		int width = trCurrent.getRegionWidth();
+		int height = trCurrent.getRegionHeight();
+
+		if (bRight)
+			spriteBatch.draw(trCurrent, body.getPosition().x - width / 4, body.getPosition().y - height / 4, width / 2, height / 2);
+		else
+			spriteBatch.draw(trCurrent, body.getPosition().x + width / 4, body.getPosition().y - height / 4, -width / 2, height / 2);
+
 		elapsedTime++;
-		if(this.state == State.IDLE) {
-			if(bRight) {
-				spriteBatch.draw(aniIdle.getKeyFrame(elapsedTime, true), body.getPosition().x - sIdle[0].getWidth() / 4, body.getPosition().y - sIdle[0].getHeight() / 4, sIdle[0].getWidth() / 2, sIdle[0].getHeight() / 2);
-			} else {
-				spriteBatch.draw(aniIdle.getKeyFrame(elapsedTime, true), body.getPosition().x + sIdle[0].getWidth() / 4, body.getPosition().y - sIdle[0].getHeight() / 4, -sIdle[0].getWidth() / 2, sIdle[0].getHeight() / 2);
-			}
-		} else if(state == State.RIGHT) {
-			spriteBatch.draw(aniRun.getKeyFrame(elapsedTime, true), body.getPosition().x - sIdle[0].getWidth() / 4, body.getPosition().y - sIdle[0].getHeight() / 4, sRun[0].getWidth() / 2, sRun[0].getHeight() / 2);
-		} else if(state == State.LEFT) {
-			spriteBatch.draw(aniRun.getKeyFrame(elapsedTime, true), body.getPosition().x + sIdle[0].getWidth() / 4, body.getPosition().y - sIdle[0].getHeight() / 4, -sRun[0].getWidth() / 2, sRun[0].getHeight() / 2);
-		}
 	}
 
 	void move() {
-		if(Gdx.input.isTouched()) {
+		if (Gdx.input.isTouched()) {
+
 			int screenHeight = Gdx.graphics.getHeight();
 			int screenWidth = Gdx.graphics.getWidth();
 			int touchX = Gdx.input.getX();
 			int touchY = Gdx.input.getY(); // Don't get near this guy ;}
 
-			if(touchX > screenWidth - (screenWidth / 3) && touchY > screenHeight - (screenHeight / 3f)) { // Bottom right
+			if (touchX > screenWidth - (screenWidth / 3) && touchY > screenHeight - (screenHeight / 3f)) { // Bottom right, move right
 				body.setLinearVelocity(100f, body.getLinearVelocity().y);
-				state = State.RIGHT;
-			} else if(touchX < (screenWidth / 3f) && touchY > screenHeight - (screenHeight / 3f)) { // Bottom left
+				bRight = true;
+				isIdle = false;
+			} else if (touchX < (screenWidth / 3f) && touchY > screenHeight - (screenHeight / 3f)) { // Bottom left, move left
 				body.setLinearVelocity(-100f, body.getLinearVelocity().y);
-				state = State.LEFT;
-			} else if(isGrounded && touchY > screenHeight - (screenHeight / 3)) { // Bottom middle
+				bRight = false;
+				isIdle = false;
+			} else if (isGrounded && touchY > screenHeight - (screenHeight / 3)) { // Bottom middle, jump
 				jump();
+			} else { // Not tapping anywhere important
+				isIdle = true;
+				stop();
 			}
-		} else {
-			state = State.IDLE;
+		} else { // Not tapping anywhere
+			isIdle = true;
 			stop();
-		}
-
-		if(Gdx.input.isKeyPressed(Input.Keys.ANY_KEY)) {
-			if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
-				body.setLinearVelocity(-100f, body.getLinearVelocity().y);
-				state = State.LEFT;
-			} else if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-				body.setLinearVelocity(100f, body.getLinearVelocity().y);
-				state = State.RIGHT;
-			} else stop();
-
-			if(Gdx.input.isKeyPressed(Input.Keys.SPACE)){
-				jump();
-			}
 		}
 	}
 
 	void stop() {
-		// stop movement on release of keycode
-		state = State.IDLE;
+		// stop movement
+		isIdle = true;
 		body.setLinearVelocity(0, body.getLinearVelocity().y);
 	}
 
